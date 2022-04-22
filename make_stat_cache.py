2e63e513d59669d66445f905f6d33209bcd4a31c
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2022-04-22 20:17:59 +0200

    Work

--- make_stat_cache.py
+++ make_stat_cache.py
@@ -3,6 +3,7 @@ import re
 from typing import NamedTuple
 import datetime
 import diskcache as dc
+import argparse
 
 
 ROOTNAME = "/nfs/nas4.irisa.fr/temp_transfert/bouge/Sauvegarde_OVH_2022-04-21/"
@@ -17,6 +18,17 @@ class File(NamedTuple):
     mod_date: float
 
 
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--root",
+        type=str,
+        dest="initial_dirname",
+    )
+    args = parser.parse_args()
+    return args
+
+
 def main():
     os.chdir(ROOTNAME)
     with dc.Cache(cachename) as cache:
@@ -24,10 +36,11 @@ def main():
 
 
 def process(cache):
-    for (i, (dirpath, dirnames, filenames)) in enumerate(
-        os.walk("Backup_1_2019_1TB/2017-05-15")
-    ):
-        if i % 10 == 0:
+    args = parse()
+    initial_dirname = args.initial_dirname
+    print(f"Starting from {initial_dirname}\n\n")
+    for (i, (dirpath, dirnames, filenames)) in enumerate(os.walk(initial_dirname)):
+        if i % 100 == 0:
             print(os.path.join(ROOTNAME, dirpath), i)
         for filename in filenames:
             path = os.path.join(dirpath, filename)
