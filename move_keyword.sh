c5b03d7af5617cfcc4c584221be040bb72142547
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2022-04-23 17:50:46 +0200

    Work

--- move_keyword.sh
+++ move_keyword.sh
@@ -17,5 +17,6 @@ export ROOT KEY
 
 mkdir -p "${ROOT}/Groups/${KEY}"
 
-find "${ROOT}" -type d -name "${KEY}" -print0 \
+find "${ROOT}"/Backup* "${ROOT}"/LACIE* "${ROOT}"/LaCie* "${ROOT}"/WD* \
+     -type d -name "${KEY}" -print0 \
     | xargs -0 -I % sh ./move.sh %
