4981045a95f10da2e497bf50d88c53d3c342e47c
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2022-04-23 17:27:04 +0200

    Work

--- move_keyword.sh
+++ move_keyword.sh
@@ -10,9 +10,10 @@ function move_key () {
     DIR="$1"
     TARGET="${ROOT}/Groups/${KEY}/${DIR}"
     BRANCH="$(dirname ${TARGET})"
+    echo DIR "==>" BRANCH
     # mkdir -p "${BRANCH}"
     # mv "${DIR}"  "${BRANCH}"
 }
 export -f move_key
 
-find "${ROOT}" -type d -iname "${KEY}" -print0 | xargs move_key 
+find "${ROOT}" -type d -iname "${KEY}" -print0 | xargs -0 move_key 
