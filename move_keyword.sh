3346ea9e4d301ef6798b564ff57823879cb24e16
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2022-04-23 17:24:51 +0200

    Work

---
+++ move_keyword.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ROOT="/nfs/nas4.irisa.fr/temp_transfert/bouge/Sauvegarde_OVH_2022-04-21"
+
+KEY="Perso"
+
+mkdir -p "${ROOT}/Groups/${KEY}"
+
+function move_key () {
+    DIR="$1"
+    TARGET="${ROOT}/Groups/${KEY}/${DIR}"
+    BRANCH="$(dirname ${TARGET})"
+    # mkdir -p "${BRANCH}"
+    # mv "${DIR}"  "${BRANCH}"
+}
+export -f move_key
+
+find "${ROOT}" -type d -iname "${KEY}" -print0 | xargs move_key 
